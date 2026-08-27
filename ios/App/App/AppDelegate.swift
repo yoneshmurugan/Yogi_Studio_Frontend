b026d00bd825f92b10882d23976f965d673f6621
yoneshmurugan <yoneshmurugan@gmail.com>
2026-08-27 15:18:42 +0530

    fix(ios): remove testing flag to enable real sms delivery

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -11,7 +11,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        Auth.auth().settings?.isAppVerificationDisabledForTesting = true
         return true
     }
 
